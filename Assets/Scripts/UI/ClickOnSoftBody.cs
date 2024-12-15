ba9671635cb39471ab66dbbf882cbc0b7efa83fb
Marc <matter.marc@outlook.com>
2024-12-15 15:24:47 +0100

    Add comments on how the UI works

--- Assets/Scripts/UI/ClickOnSoftBody.cs
+++ Assets/Scripts/UI/ClickOnSoftBody.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// For this to work, the Main Camera needs a Physics Raycaster. Then, add this script together with a Mesh Collider to a 
+/// Soft Body for which you want to update properties at runtime and set its useUI property to true.
+/// You also need a canvas with a child gameobject that has a SoftBodyUIController.
+/// </summary>
 public class ClickOnSoftBody : MonoBehaviour, IPointerDownHandler
 {
     [SerializeField]
